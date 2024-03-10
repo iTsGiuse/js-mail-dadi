c9bb6ebe591e411434174603943c9cf8c0d4b499
iTsGiuse <giuse.amato@icloud.com>
2024-03-10 23:40:37 +0100

    feat: add commenti

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -1,9 +1,12 @@
+/* GENERA UN NUMERO CASUALE PER IL GIOCATORE */
 const numeroGiocatore = parseInt(Math.random() * 5) + 1;
 console.log(numeroGiocatore);
 
+/* GENERA UN NUMERO CASUALE PER IL COMPUTER */
 const numeroComputer = parseInt(Math.random() * 5) + 1;
 console.log(numeroComputer);
 
+/* VERIFICA CONDIZIONE */
 if (numeroGiocatore > numeroComputer) {
     alert('ha vinto il giocatore'); 
 } else if (numeroGiocatore < numeroComputer) {
